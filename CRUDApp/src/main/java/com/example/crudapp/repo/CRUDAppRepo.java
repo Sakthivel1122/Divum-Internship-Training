7260814f38c83ca2dc952ab60f9c895cb0e03922
Sakthivel1122 <118536518+Sakthivel1122@users.noreply.github.com>
2023-08-07 22:24:53 +0530

    Delete CRUDApp directory

--- CRUDApp/src/main/java/com/example/crudapp/repo/CRUDAppRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.crudapp.repo;
-
-import com.example.crudapp.domain.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CRUDAppRepo extends JpaRepository<Student, Long> {
-}
