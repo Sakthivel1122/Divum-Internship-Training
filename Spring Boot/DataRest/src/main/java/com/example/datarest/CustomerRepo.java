75861ad6555d418d4331e67cfdc966d1d22fdca4
Sakthivel1122 <118536518+Sakthivel1122@users.noreply.github.com>
2023-08-07 19:38:15 +0530

    Add files via upload

---
+++ Spring Boot/DataRest/src/main/java/com/example/datarest/CustomerRepo.java
@@ -0,0 +1,8 @@
+package com.example.datarest;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "customers", path = "customers")
+public interface CustomerRepo extends JpaRepository<Customers,Integer> {
+}
