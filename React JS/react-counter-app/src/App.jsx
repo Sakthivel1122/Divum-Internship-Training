24a22dcfc2bcbd0ae6e8a16314014d84dc0381de
Sakthivel1122 <118536518+Sakthivel1122@users.noreply.github.com>
2023-08-11 14:17:38 +0530

    Add files via upload

---
+++ React JS/react-counter-app/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <h1>Count: {count}</h1>
+      <button onClick={() => setCount(count + 1)}>+</button>
+      <button onClick={() => setCount(count - 1)}>-</button>
+    </div>
+  );
+}
+
+export default App;
